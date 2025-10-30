ce6a9dc41eb93d4c4deb42ca08e39459975972f4
Michael Lill <michael.lill@tokiwa.software>
2025-10-30 11:56:52 +0100

    add tools.mk, collection of dev tools

---
+++ tools.mk
@@ -0,0 +1,38 @@
+# -----------------------------------------------------------------------
+#
+#  Tokiwa Software GmbH, Germany
+#
+#  Source code of tools.mk
+#
+#  collections of tools to ease development of fzweb
+#
+# -----------------------------------------------------------------------
+
+# usage example:
+#
+# make -f tools.mk flamegraph
+#
+
+include Makefile
+
+SHELL := /bin/bash
+
+.PHONY: bench_tutorial
+bench_tutorial:
+	ss -ltnp|grep :8080 || (echo "fzweb not running?" && exit 1)
+	for i in {1..50}; do curl -o /dev/null -s -w "%{time_total}\n" http://127.0.0.1:8080/tutorial/index; done | sort | awk '{ total += $$1; count++ } END { print "Avg:", total/count }'
+
+.PHONY: bench_root
+bench_root:
+	ss -ltnp|grep :8080 || (echo "fzweb not running?" && exit 1)
+	for i in {1..50}; do curl -o /dev/null -s -w "%{time_total}\n" http://127.0.0.1:8080/; done | sort | awk '{ total += $$1; count++ } END { print "Avg:", total/count }'
+
+.PHONY: profile
+profile:
+	printf "output will be in out.prof\n"
+	$(FUZION_BIN)/fz -XjavaProf=out.prof -jvm -JLibraries="wolfssl sodium" -verbose=2 -unsafeIntrinsics=on -modules=$(MODULES) -sourceDirs=src run
+
+.PHONY: flamegraph
+flamegraph:
+	printf "output will be in out.svg\n"
+	$(FUZION_BIN)/fz -XjavaProf=out.svg -jvm -JLibraries="wolfssl sodium" -verbose=2 -unsafeIntrinsics=on -modules=$(MODULES) -sourceDirs=src run
